e773661fa4ab95b5d8553f7bf6fc50d5d930d21c
hgcosta <hugocosta.devs@gmail.com>
2022-12-05 20:13:48 -0300

    Front-end e backend da aplicacao

--- server.js
+++ server.js
@@ -15,4 +15,19 @@ app.use('/', (req, res)=>{
     res.render('index.html');
 });
 
+
+let messages = []
+
+io.on('connection', socket =>{
+    console.log(`Socket conectado ${socket.id} `);
+
+    socket.emit('previousMessages', messages)
+
+    socket.on('sendMessage', data =>{
+        messages.push(data);
+        socket.broadcast.emit('recivedMessage', data);
+    })
+})
+
+
 server.listen(3000);
